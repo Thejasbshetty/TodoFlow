e79e0d13f8b033acb589ff13fe63fbf027881fcb
thejasbshetty <thejasbshetty@gmail.com>
2024-09-28 20:56:24 +0530

    Refactor frontend styling and add CreateTodo and Todos components

---
+++ frontend/src/components/Todos.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Todos = () => {
+  return (
+    <div>
+      <h1>Title :</h1>
+      <h2>Description: </h2>
+      <button>hello</button>
+    </div>
+  )
+}
+
+export default Todos
