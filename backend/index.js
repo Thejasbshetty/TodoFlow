a8e9e7824024149f79004446a2731db842f9c134
thejasbshetty <thejasbshetty@gmail.com>
2024-09-28 18:50:41 +0530

    Refactor backend API endpoints for creating, retrieving, and updating todos

--- backend/index.js
+++ backend/index.js
@@ -1,30 +1,50 @@
 const express = require("express");
 const { createTodo, updateTodo } = require("./types");
 const app = express();
+const { todo } = require("./database");
 
 app.use(express.json());
 
-app.post('/todo', function(req, res) {
+app.post('/todo', async function(req, res) {
   const createPayload = req.body;
   const parsedPayload = createTodo.safeParse(createPayload);
   if(!parsedPayload.success) {
      res.status(411).json({ error: "Invalid input" });
      return;
   }
+  await todo.create({
+    title: createPayload.title,
+    description: createPayload.description,
+    completed: false
+  })
+
+  res.json({
+    msg: "Todo created successfully"
+  })
 });
 
-app.get("/todos", function(req, res) {
-    
+app.get("/todos", async function(req, res) {
+    const todo = await todo.find({});
+    res.json({
+        todos: todo
+    });
 });
 
-app.put("/completed" , function(req, res) {
-    const updateTodo = req.body;
+app.put("/completed" , async function(req, res) {
+    const updatePayload = req.body;
     const parsedPayload = updateTodo.safeParse(updatePayload);
     if(!parsedPayload.success){
        res.status(411).json({ error: "Invalid input" });
        return;
     }
-
+    await todo.updateOne({
+        _id: req.body.id
+    }, {
+        completed: true
+    })
+    res.json({
+        msg: "Todo completed successfully"
+    });
 });
 
 
